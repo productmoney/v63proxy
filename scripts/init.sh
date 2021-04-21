aada0e9193c31bd4e904568bef08d7ca18dd36c8
Andrew Briscoe <everynothing@gmail.com>
2021-04-21 01:58:39 -0600

    init script prints

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,18 @@
+echo ""
+echo "Updating system and installing requirements"
+
 yum update
 
 yum install git make gcc net-tools bsdtar
 
+echo ""
+echo "Adding * hard nofile 97816 and * soft nofile 97816 to /etc/security/limits.conf"
+
 grep 97816 /etc/security/limits.conf || echo -e "* hard nofile 97816\n* soft nofile 97816" >> /etc/security/limits.conf
+
+echo ""
+echo "Init script done"
+
+echo ""
+echo "***IMPORTANT***"
+echo "Please log out and log back in before continuing"
