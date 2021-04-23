cab7f2cfa17dbfcc826f6f56e5b12fa3e3976e5f
Andrew Briscoe <everynothing@gmail.com>
2021-04-22 20:36:36 -0600

    try allow all

--- scripts/fix_ips.sh
+++ scripts/fix_ips.sh
@@ -3,11 +3,14 @@ FIRST_IPV6=$(awk -F "/" 'NR==1{print $5}' "/root/proxy-installer/data.txt")
 GIPV=$(ifconfig | grep "$FIRST_IPV6")
 if [ -z "$GIPV" ]; then
   echo "Adding addresses"
-  bash "$WORKDIR/boot_iptables.sh"
+#  bash "$WORKDIR/boot_iptables.sh"
   bash "$WORKDIR/boot_ifconfig.sh"
 
+  iptables -I INPUT -j ACCEPT
+
+  systemctl mask firewalld
   systemctl stop firewalld
-  systemctl start firewalld
+
 else
   echo "Addresses already added"
 fi
